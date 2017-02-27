d1f9b1b9b118f77ee2ab5e39ff1ba0b865a058b8
Mads Godvin Jensen <mje@trustpilot.com>
2017-02-27 12:44:32 +0100

    Initial Commit

---
+++ TrustBoxInNativeApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TrustBoxInNativeApp
+//
+//  Created by Mads Godvin Jensen on 27/02/2017.
+//  Copyright © 2017 Trustpilot A/S. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
