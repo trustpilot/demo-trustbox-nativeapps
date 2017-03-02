966b1a94e3192bb0de528657ac1c840a1c1b5f89
Mads Godvin Jensen <mje@trustpilot.com>
2017-03-02 11:24:48 +0100

    Add a tab layout and an API view

---
+++ Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/ApiFragment.java
@@ -0,0 +1,16 @@
+package com.trustpilot.trustboxinnativeapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ApiFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_api, container, false);
+        return rootView;
+    }
+}
--- Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/TrustBoxFragment.java
+++ Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/TrustBoxFragment.java
@@ -1,22 +1,27 @@
 package com.trustpilot.trustboxinnativeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class TrustBoxFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_trustbox, container, false);
+        return rootView;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         String bootstrap = "<!-- TrustBox script --> <script type=\"text/javascript\" src=\"https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js\" async></script> <!-- End Trustbox script -->";
         String trustBox = "<!-- TrustBox widget - Mini Carousel --> <div class=\"trustpilot-widget\" data-locale=\"en-US\" data-template-id=\"539ad0ffdec7e10e686debd7\" data-businessunit-id=\"55be5d980000ff000581b3e6\" data-style-height=\"350px\" data-style-width=\"100%\" data-theme=\"light\" data-stars=\"1,2,3,4,5\"> <a href=\"https://www.trustpilot.com/review/revelsystems.com\" target=\"_blank\">Trustpilot</a> </div> <!-- End TrustBox widget -->";
 
-
-        WebView webView = (WebView)findViewById(R.id.trustbox_view);
+        WebView webView = (WebView)this.getActivity().findViewById(R.id.trustbox_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadDataWithBaseURL(
                 "https://widget.trustpilot.com",
@@ -24,5 +29,6 @@ public class MainActivity extends AppCompatActivity {
                 "text/html",
                 null,
                 "https://widget.trustpilot.com");
+
     }
 }
