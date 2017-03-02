7d614eec1a5f199fc065cd4a004f8be4a06cbe2e
Mads Godvin Jensen <mje@trustpilot.com>
2017-03-02 16:27:45 +0100

    Add a drawable star svg

--- Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/ApiFragment.java
+++ Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/ApiFragment.java
@@ -1,10 +1,14 @@
 package com.trustpilot.trustboxinnativeapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class ApiFragment extends Fragment {
@@ -12,5 +16,16 @@ public class ApiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_api, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ImageView imageView = (ImageView)this.getActivity().findViewById(R.id.starsImageView);
+
+        final ContextThemeWrapper wrapper = new ContextThemeWrapper(this.getActivity(), R.style.Stars3);
+        final Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.stars, wrapper.getTheme());
+        imageView.setImageDrawable(drawable);
     }
 }
