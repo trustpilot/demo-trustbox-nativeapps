2ba8acf2f4077af546d192cddc16ab93a4ac1e63
Mads Godvin Jensen <mje@trustpilot.com>
2017-03-06 10:51:10 +0100

    Use the API to get summary data about the business unit.

--- Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/TrustBoxFragment.java
+++ Android/app/src/main/java/com/trustpilot/trustboxinnativeapp/TrustBoxFragment.java
@@ -19,7 +19,7 @@ public class TrustBoxFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         String bootstrap = "<!-- TrustBox script --> <script type=\"text/javascript\" src=\"https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js\" async></script> <!-- End Trustbox script -->";
-        String trustBox = "<!-- TrustBox widget - Mini Carousel --> <div class=\"trustpilot-widget\" data-locale=\"en-US\" data-template-id=\"539ad0ffdec7e10e686debd7\" data-businessunit-id=\"55be5d980000ff000581b3e6\" data-style-height=\"350px\" data-style-width=\"100%\" data-theme=\"light\" data-stars=\"1,2,3,4,5\"> <a href=\"https://www.trustpilot.com/review/revelsystems.com\" target=\"_blank\">Trustpilot</a> </div> <!-- End TrustBox widget -->";
+        String trustBox = "<!-- TrustBox widget - Mini Carousel --> <div class=\"trustpilot-widget\" data-locale=\"en-US\" data-template-id=\"539ad0ffdec7e10e686debd7\" data-businessunit-id=\"46d6a890000064000500e0c3\" data-style-height=\"350px\" data-style-width=\"100%\" data-theme=\"light\" data-stars=\"1,2,3,4,5\"> <a href=\"https://www.trustpilot.com/review/revelsystems.com\" target=\"_blank\">Trustpilot</a> </div> <!-- End TrustBox widget -->";
 
         WebView webView = (WebView)this.getActivity().findViewById(R.id.trustbox_view);
         webView.getSettings().setJavaScriptEnabled(true);
